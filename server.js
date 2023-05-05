a7af030d10003028c9c0f371482043c7177e30c1
sujal gupta <sujalgupta1221@gmail.com>
2023-05-05 19:55:17 +0530

    index.js file to server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config()
 
 // check for errors in route requests
-require("express-async-errors")
+// require("express-async-errors")
 
 const express = require('express')
 const app = express()
